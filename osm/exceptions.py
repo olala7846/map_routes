cc306913ca9ed7c274e39716f4ea280348d6cb60
Hsin-Cheng Chao <olala7846@gmail.com>
2021-05-22 11:08:57 -0600

    Refactor plot_osm using osm module

---
+++ osm/exceptions.py
@@ -0,0 +1,4 @@
+# exceptions.py
+
+class OsmXmlParseError(Exception):
+  pass
--- xml_analysis.py
+++ xml_analysis.py
@@ -4,16 +4,23 @@ from collections import defaultdict
 
 OSM_FILE = 'taipei.osm'
 
+
 def main():
-  tree = ET.parse(OSM_FILE)
+    tree = ET.parse(OSM_FILE)
 
-  counter = defaultdict(int)
+    counter = defaultdict(int)
 
-  for elem in tree.getroot():
-    counter[elem.tag] += 1
+    for elem in tree.getroot():
+        counter[elem.tag] += 1
 
-  print(counter)
+    # Try to answer the following questions:
+    # How many Nodes & Ways
+    print(counter)
+
+    # How many Ways are roads (have highyway tag)
+    # How many Nodes are connected to a real road
+    # Some statistic (e.g. avereage arcs per node)
 
 
 if __name__ == '__main__':
-  main()
+    main()
