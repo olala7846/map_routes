5b0331f8a56a95e423e291fd48ca520d708ef563
Hsin-Cheng Chao <olala7846@gmail.com>
2022-01-24 22:09:08 -0700

    Add plotting for compare results

--- osmnx/compare_routing_algorithms.py
+++ osmnx/compare_routing_algorithms.py
@@ -8,6 +8,7 @@ from absl import flags
 from os.path import exists
 from routing_algorithms import a_star_shortest_path
 from routing_algorithms import dijkstra_shortest_path
+import matplotlib.pyplot as plt
 import time
 import logging
 import osmnx as ox
@@ -26,6 +27,10 @@ flags.DEFINE_integer("sample_count", 10, "Number of routes to compare")
 logger = logging.getLogger("compare_routing_algorithms.py")
 
 
+def _get_node_latlon(node_id, graph):
+  node = graph.nodes[node_id]
+  lat, lon = node['y'], node['x']
+  return (lat, lon)
 
 def _sample_n_pairs(graph):
   orig_dest_pairs = []
@@ -37,13 +42,18 @@ def _sample_n_pairs(graph):
   return orig_dest_pairs
 
 def profile_algorithm(graph, orig_dest_pairs, algorithm_impl, weight="length"):
-  took_time = []
+  results = []
   for orig_id, dest_id in orig_dest_pairs:
     t_start = time.process_time()
     algorithm_impl(graph, orig_id, dest_id, weight)
     t_end = time.process_time()
-    took_time.append(t_end - t_start)
-  return took_time
+    used_time = t_end - t_start
+    orig_lat, orig_lon = _get_node_latlon(orig_id, graph)
+    dest_lat, dest_lon = _get_node_latlon(dest_id, graph)
+    distance = ox.distance.great_circle_vec(orig_lat, orig_lon, dest_lat, dest_lon)
+    results.append((distance, used_time))
+
+  return results
 
 
 def main(argv):
@@ -57,8 +67,15 @@ def main(argv):
   random.seed(42)
   orig_dest_pairs = _sample_n_pairs(G)
 
-  dijkstra_time = profile_algorithm(G, orig_dest_pairs, dijkstra_shortest_path)
-  a_star_time = profile_algorithm(G, orig_dest_pairs, a_star_shortest_path)
+  dijkstra_report = profile_algorithm(G, orig_dest_pairs, dijkstra_shortest_path)
+  a_star_report = profile_algorithm(G, orig_dest_pairs, a_star_shortest_path)
+
+  dijkstra_distance = [x[0] for x in dijkstra_report]
+  dijkstra_time = [x[1] for x in dijkstra_report]
+
+  a_star_distance = [x[0] for x in a_star_report]
+  a_star_time = [x[1] for x in a_star_report]
+
   logger.info("Finish sampling %d routes.", FLAGS.sample_count)
   logger.info(
     "Dijkstra: mean(%f) std(%f) 95%%tile(%f)",
@@ -67,6 +84,12 @@ def main(argv):
     "A-Star: mean(%f) std(%f) 95%%tile(%f)",
     np.mean(a_star_time), np.std(a_star_time), np.percentile(a_star_time, 95))
 
+  plt.scatter(dijkstra_distance, dijkstra_time, c='r', label='Dijkstra')
+  plt.scatter(a_star_distance, a_star_time, c='g', label='A*')
+  plt.xlabel('Distsance (meter)')
+  plt.ylabel('Time (seconds)')
+  plt.legend(loc='upper left')
+  plt.show()
 
 
 if __name__ == '__main__':
